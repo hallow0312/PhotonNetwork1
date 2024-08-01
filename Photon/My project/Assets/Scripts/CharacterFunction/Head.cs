3cecaa94ccfaf4f492256620f4d6adaad153f2b5
hallow0312 <leejaehyun0312@naver.com>
2024-08-01 21:47:07 +0900

    2024.08.01.카메라흔들림 시스템 추가

--- Photon/My project/Assets/Scripts/CharacterFunction/Head.cs
+++ Photon/My project/Assets/Scripts/CharacterFunction/Head.cs
@@ -10,14 +10,28 @@ public class Head : MonoBehaviourPunCallbacks
    
     [SerializeField] float x;
     [SerializeField] float speed=200.0f;
-  
-
+    
+    [SerializeField] float amount =5.0f;
+    
     public void Update()
     {
         if (photonView.IsMine == false) return;
      
-         x= -Input.GetAxis("Mouse Y")*speed *Time.deltaTime;
+        x+= -Input.GetAxis("Mouse Y")*speed *Time.deltaTime;
         x = Mathf.Clamp(x, -90, 90);
-        transform.eulerAngles = new Vector3(x, 0, 0);
+        transform.localEulerAngles = new Vector3(x, 0, 0);
+    }
+
+    public IEnumerator Shake(float timer)
+    {
+       
+        Vector3 StartPosition = transform.position;
+        while (timer > 0)
+        {
+            transform.localPosition = Random.insideUnitSphere * amount + StartPosition;
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = StartPosition;
     }
 }
--- Photon/My project/Assets/Scripts/CharacterFunction/Mouse.cs
+++ Photon/My project/Assets/Scripts/CharacterFunction/Mouse.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
     [SerializeField] LayerMask layermask;
     [SerializeField] Ray ray;
+    [SerializeField] RaycastHit hit;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Transform rayposition;
     // Start is called before the first frame update
     void Start()
     {
        ActiveMouse(false, CursorLockMode.Locked);
+      
     }
 
     public void Update()
     {
         if(Input.GetButton("Fire1"))
         {
+           
+            direction = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
+            ray= new Ray(rayposition.position, direction);
 
+            if(Physics.Raycast(ray, out hit,Mathf.Infinity,layermask))
+            {
+                
+            }
+           
         }
     }
 
@@ -24,5 +37,11 @@ public class Mouse : MonoBehaviour
     {
         Cursor.visible = state;
         Cursor.lockState = mode;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(rayposition.position, direction * 100);
     }
 }
